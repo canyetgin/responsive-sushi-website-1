d8b6eef01dc35ecf3d29cfc1f47c094ea3b03dbf
canyetgin <cannyetgin@gmail.com>
2022-12-08 19:47:48 +0300

    navbar-small-screen

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu')
+})
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu')
+})
