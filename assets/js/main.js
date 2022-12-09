efed7449f477a9f3130d4ff44f767613a61c307d
canyetgin <cannyetgin@gmail.com>
2022-12-09 10:50:24 +0300

    dark/light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,32 @@ sr.reveal('.home-data', { origin: 'bottom' })
 sr.reveal('.about-data, .recently-data', { origin: 'left' })
 sr.reveal('.about-img, .recently-img', { origin: 'right' })
 sr.reveal('.popular-card', { intervar: 100 })
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](
+    iconTheme
+  )
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
