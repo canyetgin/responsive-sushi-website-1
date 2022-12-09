0ff04d687bcb0c2838f7e691dfbd8ebcc5906946
canyetgin <cannyetgin@gmail.com>
2022-12-09 08:58:28 +0300

    active link auto

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ navClose.addEventListener('click', () => {
   navMenu.classList.remove('show-menu')
 })
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav-link')
 
 const linkAction = () => {
   const navMenu = document.getElementById('nav-menu')
@@ -31,10 +31,31 @@ window.addEventListener('scroll', scrolHeader)
 const scrollUp = () => {
   const scrollUp = document.getElementById('scroll-up')
   if (this.scrollY >= 350) {
-    scrollUp.style="display:flex;"
+    scrollUp.style = 'display:flex;'
   } else {
     scrollUp.style = 'display:none;'
   }
 }
 
 window.addEventListener('scroll', scrollUp)
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector(
+        '.nav-menu a[href*=' + sectionId + ']'
+      )
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
